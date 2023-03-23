a446235e2615c95ba98952f2aec192c4474e0654
nw51631 <127346229+nw51631@users.noreply.github.com>
2023-03-23 13:52:38 +0000

    Add files via upload

---
+++ AlgorithmsAndComplexityAssignment1-master/AlgorithmsAndComplexityAssignment1-master/AlgorithmsAndComplexityAssignment1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AlgorithmsAndComplexityAssignment1
+{
+    class Program
+    {
+        
+
+		public static void Main(string[] args)
+		{
+
+			string[] lines = File.ReadAllLines("c:\\2981757.txt");
+			//add open with txt files
+			int[] values = new int[465];
+			Console.WriteLine("Not sorted:");
+		
+			
+
+
+
+
+
+			//bubble Sorting
+			int[] bubbled = Sort.bubble(values);
+			Console.WriteLine("Bubble Sorted:");
+			for (int i = 0; i < bubbled.Length; i++)
+			{
+				Console.WriteLine(bubbled[i]);
+			}
+			//sort into every 10th value 
+			int[] selected = Sort.selection(values);
+			Console.WriteLine("Sorted:");
+			for (int i = 0; i < selected.Length; i++)
+			{
+				Console.WriteLine(selected[i]);
+			}
+			//Insert sorting
+			int[] inserted = Sort.insertion(values);
+			Console.WriteLine("Insertion Sorted:");
+			for (int i = 0; i < inserted.Length; i++)
+			{
+				Console.WriteLine(bubbled[i]);
+			}
+			
+			Console.ReadLine();
+		}
+
+	}
+}
---
+++ AlgorithmsAndComplexityAssignment1-master/AlgorithmsAndComplexityAssignment1-master/AlgorithmsAndComplexityAssignment1/Sort.cs
@@ -0,0 +1,66 @@
+﻿using System;
+namespace AlgorithmsAndComplexityAssignment1
+{
+	public static class Sort
+	{
+		
+		public static int[] bubble(int[] array)
+		{
+			
+			for (int i = 0; i < array.Length; i++)
+			{
+				
+				for (int j = 1; j < array.Length; j++)
+				{
+					
+					if (array[j - 1] > array[j])
+					{
+						
+						int temp = array[j];
+						array[j] = array[j - 1];
+						
+						array[j - 1] = temp;
+					}
+				}
+			}
+			return array;
+		}
+		
+		public static int[] selection(int[] array)
+		{
+			for (int i = array.Length - 1; i > 0; i--)
+			{
+				int high = array[0];
+				int index = 0;
+
+				for (int j = 0; j < i; j++)
+				{
+					if (array[j] > high)
+					{
+						high = array[j];
+						index = j;
+					}
+				}
+			}
+			return array;
+		}
+		
+		public static int[] insertion(int[] array)
+		{
+			
+			for (int i = 1; i < array.Length; i++)
+			{
+				int index = array[i];
+				int j = i;
+				
+				while ((j > 0) && (array[j - 1] > index))
+				{
+					array[j] = array[j - 1];
+					j = j - 1;
+				}
+				array[j] = index;
+			}
+			return array;
+		}
+	}
+}
